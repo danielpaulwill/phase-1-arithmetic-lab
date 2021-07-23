eb4b02fbea01f9585ac6771a4cc8bf3c63f67997
Daniel Williams <danielpaulwill@gmail.com>
2021-07-22 20:30:06 -0400

    commit tree 2036caadd367647e6d1f61d9226b140d37d9a527

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let multiply
+const num1 = 2
+const num2 = 31
+multiply = num1 * num2;
 
-// Write your code here
+
+let random
+random = Math.ceil(Math.random() * 13);
+
+
+let mod
+const num3 = 24
+const num4 = 5
+mod = 24 % 5;
+
+
+let max
+max = Math.max(4,8,12,16,20);
